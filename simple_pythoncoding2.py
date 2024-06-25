ca4fa3deec9b0b388ba604c9b812e24de11c275c
vamsikrishnakanth <vamsikrishnakanth5991@gmail.com>
2024-06-25 16:49:15 +0530

    PythonCoding

---
+++ simple_pythoncoding2.py
@@ -0,0 +1,12 @@
+print("Hello world") #print is used to print the given variable in the terminal
+name,age,height="vamsi",10,11.9 # we can write the all the variable in a single statement unlike java, we don't need to specify the Type
+print(str(name))# we need do Type casting for Each and every variable irrespective of String and int , flot
+print(str(age))
+print (str(height))
+print(type(name))# these is used to determine the type of the variable.
+print(type(height))
+print(type(age)) 
+myName="Bro code"
+print(len(name))# in a String we can determine the lengthy of the string
+print(name.find("o"))# in a String if we need to find the position of word we use "Stringname.find("required value")"
+print(name.capitalize) 
