c477ca5c02ddf7d4eb1856059790293c7f6c211a
vamsikrishnakanth <vamsikrishnakanth5991@gmail.com>
2024-06-25 17:29:03 +0530

    Create math_Keyword_&_Operations.py

---
+++ math_Keyword_&_Operations.py
@@ -0,0 +1,18 @@
+import math
+x=1232.455555 # Given number is float value
+y=1122.33
+z=1229
+print(type(x)) #To find the DataType  <class 'float'>
+print(round(x)) #To find the Overall or Round figure number of X.
+print(x) 
+print(math.ceil(x))
+print(x)
+print(math.floor(x))
+print(x)
+print(abs(x)) #To find the abslout value of the Number. In case we pass Negative number in the x we get a Positive number
+print(x)
+print(pow(x,2)) #To Raise base value to the power number print(pow(base,Exponent))
+print(x)
+print(max(x,y,z)) #To find the maximum value and variables
+print(min(x,y,z))# To find minimum value
+
