d3e71df53417a1161ba44e3377aacc18e8ef6023
vamsikrishnakanth <vamsikrishnakanth5991@gmail.com>
2024-07-01 22:20:55 +0530

    Create two_numbers_are_Equal.py

---
+++ two_numbers_are_Equal.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter the number :"))
+n2=int(input("Enter the second number :"))
+
+if n1==n2:
+    print("the given two numbers are equal"+n1+","+n2)
+elif n1>n2 :
+    print("The n1 is greater n2"+ n1)
+else:
+    print("the n2 is greater than n1"+ n2)
+
