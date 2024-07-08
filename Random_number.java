012b851f2e79c46f40cfad74f8ab6c356d3db9e7
vamsikrishnakanth <vamsikrishnakanth5991@gmail.com>
2024-07-08 16:35:32 +0530

    Create Random_number.java

---
+++ Random_number.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class SecondClass {
+    public static void main(String[] args) {
+        Random random=new Random();
+        int x=random.nextInt();
+        System.out.println("Random Number"+":"+x);
+     
+    }
+}
